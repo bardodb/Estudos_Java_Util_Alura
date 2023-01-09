1fb5df144d189033745f2b6a455852048786c8c0
Braian Sins <bsins198@gmail.com>
2023-01-09 16:10:30 -0300

    List/ArrayList/LinkedList

--- src/br/com/bytebank/banco/test/TesteArrayList.java
+++ src/br/com/bytebank/banco/test/TesteArrayList.java
@@ -4,24 +4,46 @@ import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.GuardadordeReferencias;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class TesteArrayList {
 
     public static void main(String[] args) {
 
-
-      GuardadordeReferencias guardador = new GuardadordeReferencias();
+//generics
+      List<Conta> lista = new ArrayList<>();
 
       Conta cc = new ContaCorrente(22, 11);
-      guardador.adiciona(cc);
+      lista.add(cc);
 
-      Conta cc2 = new ContaCorrente(22, 22);
-      guardador.adiciona(cc2);
+      Conta cc2 = new ContaCorrente(22, 11);
+      lista.add(cc2);
 
-      int tamanho = guardador.getQuantidadeDeElementos();
-      System.out.println(tamanho);
-
-      Conta ref = guardador.getReferencia(0);
+      System.out.println("Tamanho: " + lista.size());
+      Conta ref = lista.get(0);
       System.out.println(ref.getNumero());
+
+      lista.remove(0);
+
+      System.out.println("Tamanho: " +lista.size());
+
+      Conta cc3 = new ContaCorrente(33, 311);
+      lista.add(cc3);
+
+      Conta cc4 = new ContaCorrente(33, 322);
+      lista.add(cc4);
+      for (int i =0; i < lista.size(); i++){
+        Object oRef = lista.get(i);
+        System.out.println(oRef);
+      }
+      System.out.println("-----------");
+
+      for (Conta conta : lista ){
+        System.out.println(conta);
+      }
+
 
     }
 
