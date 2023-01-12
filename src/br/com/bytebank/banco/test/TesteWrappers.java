c89c982994e3e9071f0830372458c4eac83989b0
BraianSins <bsins198@gmail.com>
2023-01-12 11:25:24 -0300

    att

--- src/br/com/bytebank/banco/test/TesteWrappers.java
+++ src/br/com/bytebank/banco/test/TesteWrappers.java
@@ -1,5 +1,8 @@
 package br.com.bytebank.banco.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TesteWrappers {
   public static void main(String[] args) {
 
@@ -13,7 +16,15 @@ public class TesteWrappers {
     Boolean bRef = Boolean.TRUE;
     System.out.println(bRef.booleanValue());
 
-    Number refNumero = Integer.valueOf(29);
+    //Number refNumero = Integer.valueOf(29);
+
+    Number refNumero = Float.valueOf(29.0f);
+
+
+    List<Number> lista = new ArrayList<>(); //number engloba todos java.lang.*      
+            lista.add(10);
+            lista.add(32.6);
+            lista.add(25.6f);
 
   }
 
